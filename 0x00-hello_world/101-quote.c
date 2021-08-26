0f094523f5342da50d491c818e0f642a6be4fdbc
MickyBe <bmikiyas@gmail.com>
2021-08-26 22:38:21 +0300

    first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,21 +1,13 @@
-/*
- * File: 101-quote.c
- * Auth: Brennan D Baraban
- */
-
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *                followed by a new line, to standard error.
+ * main - Entry point
  *
- * Return: Always 1.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
